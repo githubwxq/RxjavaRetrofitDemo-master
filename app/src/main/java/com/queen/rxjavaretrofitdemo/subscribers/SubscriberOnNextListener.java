642330701237f226ce6582c9f0992732ad66ed2c
805380422@qq.com <805380422@qq.com>
2016-07-01 11:19:33 +0800

    注释

--- app/src/main/java/com/queen/rxjavaretrofitdemo/subscribers/SubscriberOnNextListener.java
+++ app/src/main/java/com/queen/rxjavaretrofitdemo/subscribers/SubscriberOnNextListener.java
@@ -4,5 +4,5 @@ package com.queen.rxjavaretrofitdemo.subscribers;
  * Created by liukun on 16/3/10.
  */
 public interface SubscriberOnNextListener<T> {
-    void onNext(T t);
+ void onNext(T t);// 给需要数据的类实现的随便用
 }
