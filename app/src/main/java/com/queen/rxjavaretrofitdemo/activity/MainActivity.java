fc8cdb493cb06d5b465c035c7616752928487231
805380422@qq.com <805380422@qq.com>
2016-07-01 11:21:02 +0800

    mainactivity新理解

--- app/src/main/java/com/queen/rxjavaretrofitdemo/activity/MainActivity.java
+++ app/src/main/java/com/queen/rxjavaretrofitdemo/activity/MainActivity.java
@@ -6,10 +6,10 @@ import android.widget.Button;
 import android.widget.TextView;
 
 import com.queen.rxjavaretrofitdemo.R;
-import com.queen.rxjavaretrofitdemo.subscribers.ProgressSubscriber;
-import com.queen.rxjavaretrofitdemo.subscribers.SubscriberOnNextListener;
 import com.queen.rxjavaretrofitdemo.entity.Subject;
 import com.queen.rxjavaretrofitdemo.http.HttpMethods;
+import com.queen.rxjavaretrofitdemo.subscribers.ProgressSubscriber;
+import com.queen.rxjavaretrofitdemo.subscribers.SubscriberOnNextListener;
 
 import java.util.List;
 
@@ -24,7 +24,7 @@ public class MainActivity extends AppCompatActivity {
     @Bind(R.id.result_TV)
     TextView resultTV;
 
-    private SubscriberOnNextListener getTopMovieOnNext;
+    private SubscriberOnNextListener getTopMovieOnNext;//其实完全可以用当类实现接口获取onnext中的数据t
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,7 +35,9 @@ public class MainActivity extends AppCompatActivity {
         getTopMovieOnNext = new SubscriberOnNextListener<List<Subject>>() {
             @Override
             public void onNext(List<Subject> subjects) {
+
                 resultTV.setText(subjects.toString());
+
             }
         };
     }
@@ -59,6 +61,8 @@ public class MainActivity extends AppCompatActivity {
 
     //进行网络请求
     private void getMovie(){
+
+       //!!!! getTopMovieOnNext 监听progressSubscirber中的数据 获取onnext中参数数据 而 Subscriber 本质上也实现了 接口  被订阅者 触发事件然后 subscribe 就能接收到数据了onnext 中的数据 t  数据用通过回调给了activity
         HttpMethods.getInstance().getTopMovie(new ProgressSubscriber(getTopMovieOnNext, MainActivity.this), 0, 10);
     }
 }
